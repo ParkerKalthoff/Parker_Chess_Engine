544fa499ff53ec553d578325020e7e328ebcb25c
Parker Kalthoff <kalth026@morris.umn.edu>
2024-07-18 00:03:07 -0500

    w

--- test.py
+++ test.py
@@ -1,72 +1,36 @@
-def generate_bishop_moves():
-    bishop_moves = {}
-
-    for row in range(8):
-        for col in range(8):
-            bitboard = 0
-
-            for direction in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-                r, c = row, col
-                while 0 <= r + direction[0] < 8 and 0 <= c + direction[1] < 8:
-                    r += direction[0]
-                    c += direction[1]
-                    position_index = r * 8 + c
-                    bitboard |= (1 << position_index)
-
-            bishop_moves[(row, col)] = bitboard
-
-    return bishop_moves
-
-def generate_rook_moves():
-    rook_moves = {}
-
-    for row in range(8):
-        for col in range(8):
-            bitboard = 0
-
-            for direction in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                r, c = row, col
-                while 0 <= r + direction[0] < 8 and 0 <= c + direction[1] < 8:
-                    r += direction[0]
-                    c += direction[1]
-                    position_index = r * 8 + c
-                    bitboard |= (1 << position_index)
-
-            rook_moves[(row, col)] = bitboard
-
-    return rook_moves
-
-def generate_queen_moves():
-    queen_moves = {}
-
-    for row in range(8):
-        for col in range(8):
-            bitboard = 0
-
-            for direction in [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1)]:
-                r, c = row, col
-                while 0 <= r + direction[0] < 8 and 0 <= c + direction[1] < 8:
-                    r += direction[0]
-                    c += direction[1]
-                    position_index = r * 8 + c
-                    bitboard |= (1 << position_index)
-
-            queen_moves[(row, col)] = bitboard
-
-    return queen_moves
-
-def print_moves_as_java_array(moves, piece_type):
-    print(f"long[] {piece_type}Moves = new long[] {{")
-    for position, bitboard in moves.items():
-        print(f"    0x{bitboard:016x}L,")
-    print("};")
-
-if __name__ == "__main__":
-    bishop_moves = generate_bishop_moves()
-    rook_moves = generate_rook_moves()
-    queen_moves = generate_queen_moves()
-
-    print_moves_as_java_array(bishop_moves, "bishop")
-    print_moves_as_java_array(rook_moves, "rook")    
-    print_moves_as_java_array(queen_moves, "queen")
+def get_king_moves(position):
+    """
+    Get the list of indices of possible king moves from the given position.
     
+    :param position: The current position of the king (0-63).
+    :return: A list of possible move indices for the king.
+    """
+    def is_valid_index(index):
+        return 0 <= index < 64
+
+    def same_row(index1, index2):
+        return index1 // 8 == index2 // 8
+
+    moves = []
+    row, col = divmod(position, 8)
+    possible_moves = [
+        (row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+        (row, col - 1),                 (row, col + 1),
+        (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)
+    ]
+    
+    for r, c in possible_moves:
+        if 0 <= r < 8 and 0 <= c < 8:
+            moves.append(r * 8 + c)
+    
+    return moves
+
+s = '{'
+for i in range(64):
+    num = 0
+    for move in get_king_moves(i):
+        num += 1 << move
+    s += f'{hex(num)}L,'
+s += '}'
+
+print(s)
