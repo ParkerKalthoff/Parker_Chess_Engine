da366ed4b843fd9b04efee14194d144bd81c2f6c
ParkerKalthoff <parkerlkalthoff@gmail.com>
2024-07-18 23:36:37 -0500

    w

--- parkerfish_v1/src/main/java/parkerfish_v1/Main.java
+++ parkerfish_v1/src/main/java/parkerfish_v1/Main.java
@@ -1,23 +1,28 @@
 package parkerfish_v1;
 
-public class Main {
+public class Main extends bitboard{
     public static void main(String[] args) {
-
-    
-        printBitboard(0xf0f0f0f0f0f0f0f0L);
+        printBitboard(bit_after[A4][C6]);
 
     }
-    public static void printBitboard(long bitboard) {
+    public static void printBitboard(long bitboard, boolean with_coords) {
         for (int rank = 7; rank >= 0; rank--) {  // Start from the top rank (rank 7)
+            System.out.print(with_coords ? (""+(rank+1)+"   ") : "");
             for (int file = 0; file < 8; file++) {  // Iterate through each file in the rank
                 int square = rank * 8 + file;
                 long mask = 1L << square;
                 // Print 1 if the bit at the current square is set, otherwise print 0
                 System.out.print((bitboard & mask) != 0 ? "1 " : "0 ");
             }
-            System.out.println();  // Move to the next line after printing a rank
+            System.out.println();
+              // Move to the next line after printing a rank
         }
-        System.out.println();  // Add an extra line for better readability
+        System.out.println(with_coords ? "    A B C D E F G H" : "");
+
+    }
+
+    public static void printBitboard(long bitboard){
+        printBitboard(bitboard, false);
     }
     
 
